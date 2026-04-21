e9974afbfebe88b511655b0f621825926b25d440
Alan_078 <Ls23120218@surguanajuato.tecnm.mx>
2026-04-21 01:41:29 -0600

    Cambio de direccionamiento de trafico en la api

--- frontend/lib/features/favoritos/logic/favoritos_controller.dart
+++ frontend/lib/features/favoritos/logic/favoritos_controller.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../../../services/auth_service.dart';
 
-const String _baseUrl = 'http://10.0.2.2:3000/api';
+const String _baseUrl = 'http://192.168.100.5:3000/api';
 
 class FavoritosController extends ChangeNotifier {
   bool isLoading = false;
