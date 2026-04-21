95b6dc72593f094a21b982b217b11b616ab5ef8a
Alan_078 <Ls23120218@surguanajuato.tecnm.mx>
2026-04-21 02:03:52 -0600

    Cambio de nombre para la app -> ComeSUR

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.frontend"
+    namespace = "com.example.ComeSUR"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
